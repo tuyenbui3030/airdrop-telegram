3aa833fa5df477aacca40c8ada0448369d390435
tuyenbui3030 <48544028+tuyenbui3030@users.noreply.github.com>
2024-09-17 13:05:15 +0700

    feat: #2 - Update daily farming

--- blum.go
+++ blum.go
@@ -456,7 +456,7 @@ func main() {
 
 	dailyResult, err := claimDailyReward(token)
 	if err != nil {
-		log.Fatal("🚨 Error daily farming:", err)
+		fmt.Printf("🚨 Error daily farming:", err)
 	}
 
 	fmt.Printf("👋 Hello, %s!\n", username)
